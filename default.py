782d153c030fb3259eaa66bb67edd99822970c4b
Nikii <billabonq___@hotmail.de>
2022-02-15 13:21:19 +0100

    testing resolver import

--- default.py
+++ default.py
@@ -4,5 +4,5 @@ import sys, xbmc, xbmcgui
 import resolveurl as resolver
 #import urlresolver as resolver
 
-sLink = resolver.resolve(str('https://anicloud.io/redirect/775490'))
+sLink = resolver.resolve(str('https://voe.sx/e/q71l8ub1kp3b'))
 xbmc.executebuiltin('PlayMedia('+sLink+')')
