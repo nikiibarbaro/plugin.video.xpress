169c4606cc01aa9fc1020f82ba012b4bb1ee6041
Nikii <billabonq___@hotmail.de>
2022-02-17 17:46:34 +0100

    fixed import

--- default.py
+++ default.py
@@ -2,14 +2,14 @@
 
 import sys, xbmc, xbmcgui
 
-resolverAddonID = 'script.module.resolveurl'
-resolverAddon= xbmcaddon.Addon(resolverAddonID)
-addonResolverTranslatedPath = translatePath(resolverAddon.getAddonInfo('path'))
-
-path.append(addonResolverTranslatedPath)
-
-import resolveurl as resolver
+# resolverAddonID = 'script.module.resolveurl'
+# resolverAddon= xbmcaddon.Addon(resolverAddonID)
+# addonResolverTranslatedPath = translatePath(resolverAddon.getAddonInfo('path'))
+#
+# path.append(addonResolverTranslatedPath)
+#
+# import resolveurl as resolver
 #import urlresolver as resolver
 
-sLink = resolver.resolve(str('https://voe.sx/e/q71l8ub1kp3b'))
-xbmc.executebuiltin('PlayMedia('+sLink+')')
+# sLink = resolver.resolve(str('https://voe.sx/e/q71l8ub1kp3b'))
+# xbmc.executebuiltin('PlayMedia('+sLink+')')
