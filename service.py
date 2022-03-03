b15271dc3d8b0a260c0314ff40267306287e99bf
nikiibarbaro <13036889+nikiibarbaro@users.noreply.github.com>
2022-03-03 13:34:25 +0100

    changed how updates will be treated

--- service.py
+++ service.py
@@ -2,8 +2,6 @@ import xbmcgui
 import xbmc
 import xbmcaddon
 import xbmcvfs
-from os.path import join
-from sys import path
 
 from os.path import join
 from sys import path
@@ -14,11 +12,18 @@ ADDON_NAME = "plugin.video.xpress"
 import web_pdb;
 web_pdb.set_trace()
 
-addonXpress = xbmcaddon.Addon(ADDON_NAME)
-xpressAddonPath = xbmcvfs.translatePath(addonXpress.getAddonInfo('path'))
-path.append(xpressAddonPath)
-path.append(join(xpressAddonPath, "resources", "lib", "utils"))
-from resources.lib.utils.updateManager import updateManager
+#addonXpress = xbmcaddon.Addon(ADDON_NAME)
+#xpressAddonPath = xbmcvfs.translatePath(addonXpress.getAddonInfo('path'))
+#path.append(xpressAddonPath)
+#path.append(join(xpressAddonPath, "resources", "lib", "controllers"))
+
+
+addonPath = xbmcvfs.translatePath(xbmcaddon.Addon(ADDON_NAME).getAddonInfo('path'))
+path.append(join(addonPath, "resources", "lib", "controllers"))
+from resources.lib.controllers.add_paths import addPaths
+
+from resources.lib.controllers.update_manager import updateManager
+from resources.lib.controllers.logger import Logger
 
 """
     Stopping addon just for safty to not conflict with potential updates
@@ -28,29 +33,14 @@ xbmc.executebuiltin("StopScript(addon)")
 """Create dialog object for displaying informations"""
 dialog = xbmcgui.Dialog()
 
-test = updateManager.isUpdate()
-if (test is None):
-    dialog.notification('xPress', 'Ein Fehler beim Updateprozess trat auf', xbmcgui.NOTIFICATION_INFO, 5000, True)
-elif (test):
-    dialog.notification('xPress', 'Ein neues Update wurde gefunden', xbmcgui.NOTIFICATION_INFO, 5000, True)
+updateState = updateManager.isUpdate()
+if (updateState is None):
+    dialog.notification('xPress', 'Updateprozess wurde abgebrochen, siehe Details: kodi.log', xbmcgui.NOTIFICATION_INFO, 5000, True)
+elif (updateState):
+    dialog.notification('xPress', 'Ein neues Update ist verfügbar und wird installiert', xbmcgui.NOTIFICATION_INFO, 5000, True)
+    xbmc.executebuiltin("UpdateAddonRepos")
 else:
-    dialog.notification('xPress', 'Keine neuen Updates gefunden', xbmcgui.NOTIFICATION_INFO, 5000, True)
+    dialog.notification('xPress', 'Keine neuen Updates verfügbar', xbmcgui.NOTIFICATION_INFO, 5000, True)
 
-updateManager.disableAddon()
-dialog.notification('xPress', 'Test', xbmcgui.NOTIFICATION_INFO, 5000, True)
-dialog.notification('xPress', 'Test', xbmcgui.NOTIFICATION_INFO, 5000, True)
-dialog.notification('xPress', 'Test', xbmcgui.NOTIFICATION_INFO, 5000, True)
-updateManager.enableAddon()
-"""Download .zip file"""
-# response = requests.get(url, stream=True)
-# with open('E:\\test.zip', 'wb') as fd:
-#     for chunk in response.iter_content(chunk_size=512):
-#         fd.write(chunk)
-#
-#
-#
-#
-# dialog.ok('', url)
-#
-#
-# dialog.notification('xPress', 'Add-on wurde erfolgreich geupdated', xbmcgui.NOTIFICATION_INFO, 5000, True)
+#Logger.info("Test")
+
