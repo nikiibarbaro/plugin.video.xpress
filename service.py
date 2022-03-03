f12894026f226c213f47e3b4a5f583e9b4fc0c05
nikiibarbaro <13036889+nikiibarbaro@users.noreply.github.com>
2022-03-03 14:21:45 +0100

    test version increment

--- service.py
+++ service.py
@@ -9,8 +9,8 @@ from sys import path
 ADDON_NAME = "plugin.video.xpress"
 #import pydevd
 #pydevd.settrace('localhost', port=1234, stdoutToServer=True, stderrToServer=True)
-import web_pdb;
-web_pdb.set_trace()
+#import web_pdb;#NEED TO COMMENTED OUT BEFORE PUSHING TO GITHUB TO PREVENT UPDATER BREAKS
+#web_pdb.set_trace()#NEED TO COMMENTED OUT BEFORE PUSHING TO GITHUB TO PREVENT UPDATER BREAKS
 
 #addonXpress = xbmcaddon.Addon(ADDON_NAME)
 #xpressAddonPath = xbmcvfs.translatePath(addonXpress.getAddonInfo('path'))
@@ -38,11 +38,7 @@ if (updateState is None):
     dialog.notification('xPress', 'Updateprozess wurde abgebrochen, siehe Details: kodi.log', xbmcgui.NOTIFICATION_INFO, 5000, True)
 elif (updateState):
     dialog.notification('xPress', 'Ein neues Update ist verfügbar und wird installiert', xbmcgui.NOTIFICATION_INFO, 5000, True)
-    xbmc.executebuiltin("UpdateAddonRepos")
-    with web_pdb.catch_post_mortem():
-        # Some error-prone code
-        web_pdb.c
-
+    xbmc.executebuiltin("UpdateAddonRepos")#After this line code breaks when debug mode is on with pdb web
 else:
     dialog.notification('xPress', 'Keine neuen Updates verfügbar', xbmcgui.NOTIFICATION_INFO, 5000, True)
 #Logger.info("Test")
