6241bf9a2870ee5b8dad37526860875b88012853
nikiibarbaro <13036889+nikiibarbaro@users.noreply.github.com>
2022-03-04 21:26:50 +0100

    small changes

--- update_addon_repo.py
+++ update_addon_repo.py
@@ -3,6 +3,7 @@ import xbmcgui
 
 ADDON_ID = "plugin.video.xpress"
 dialog = xbmcgui.Dialog()
+dialog.notification('xPress', 'Ein neues Update ist verfügbar und wird installiert', xbmcgui.NOTIFICATION_INFO, 5000, True)
 xbmc.executebuiltin("StopScript({0})".format(ADDON_ID))
 xbmc.executebuiltin("UpdateAddonRepos")
 xbmc.executebuiltin("UpdateLocalAddons")
