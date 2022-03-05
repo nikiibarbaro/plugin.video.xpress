72fb809d4d792140f070dcd1bd0b2ad53270932a
nikiibarbaro <13036889+nikiibarbaro@users.noreply.github.com>
2022-03-05 17:26:40 +0100

    changed update algorithm

--- resources/lib/controllers/update_manager.py
+++ resources/lib/controllers/update_manager.py
@@ -1,9 +1,11 @@
 import xbmcaddon
 import xbmcvfs
 import xbmc
-import requests #normal to be not recognized by IDEA due packages not exists in env
+import requests  # normal to be not recognized by IDEA due packages not exists in env
 import re
 import os
+import sqlite3
+import datetime
 import xml.etree.ElementTree as et
 
 from os.path import join
@@ -23,17 +25,18 @@ ADDON_PATH = "zips/plugin.video.xpress"
 """special protocol variable for multi OS support"""
 pathHome = xbmcvfs.translatePath("special://home")
 pathXbmc = xbmcvfs.translatePath("special://xbmc")
-pathPackages = xbmcvfs.translatePath(join("special://home","addons", "packages"))
-#pathAddon = xbmcaddon.Addon(ADDON_NAME).getAddonInfo("path")
+pathPackages = xbmcvfs.translatePath(join("special://home", "addons", "packages"))
+pathDatabase = xbmcvfs.translatePath(join("special://home", "userdata", "Database", "Addons33.db"))
+# pathAddon = xbmcaddon.Addon(ADDON_NAME).getAddonInfo("path")
 
 """Add filepath from imports to addon"""
-#addonPath = xbmcvfs.translatePath(xbmcaddon.Addon(ADDON_NAME).getAddonInfo('path'))
-#path.append(join(addonPath, "resources", "lib", "controllers"))
-#addonPath = xbmcvfs.translatePath(xbmcaddon.Addon(ADDON_NAME).getAddonInfo('path'))
-#xpressAddonPath = xbmcvfs.translatePath(addonXpress.getAddonInfo('path'))
-#path.append(addonPath)
-#path.append(join(addonPath, "resources", "lib", "modules"))
-#from resources.lib.controllers.addPaths import addPaths
+# addonPath = xbmcvfs.translatePath(xbmcaddon.Addon(ADDON_NAME).getAddonInfo('path'))
+# path.append(join(addonPath, "resources", "lib", "controllers"))
+# addonPath = xbmcvfs.translatePath(xbmcaddon.Addon(ADDON_NAME).getAddonInfo('path'))
+# xpressAddonPath = xbmcvfs.translatePath(addonXpress.getAddonInfo('path'))
+# path.append(addonPath)
+# path.append(join(addonPath, "resources", "lib", "modules"))
+# from resources.lib.controllers.addPaths import addPaths
 from resources.lib.modules.packaging import version
 
 
@@ -42,17 +45,22 @@ class updateManager:
 
     @staticmethod
     def getLocalVersion():
-        addonVersionLocal = xbmcaddon.Addon(ADDON_NAME).getAddonInfo("version")
+        try:
+            addonVersionLocal = xbmcaddon.Addon(ADDON_NAME).getAddonInfo("version")
+        except RuntimeError:
+            xbmc.sleep(4000)
+            addonVersionLocal = xbmcaddon.Addon(ADDON_NAME).getAddonInfo("version")
         if (addonVersionLocal is None):
             return None
         return addonVersionLocal
 
     """Get version from remote zip"""
+
     @staticmethod
     def getRemoteVersion():
         url = "https://raw.githubusercontent.com/{0}/{1}/master/zips/addons.xml".format(USER, REPOSITORY_NAME)
         response = requests.get(url)
-        if(response.status_code != 200):
+        if (response.status_code != 200):
             return None
         tree = et.fromstring(response.content)
         for element in tree:
@@ -60,6 +68,43 @@ class updateManager:
                 remoteVersion = element.attrib.get("version")
                 return remoteVersion
         return None
+
+    @staticmethod
+    def forceRepoUpdate():
+
+        xbmc.sleep(1000)
+
+        connection = sqlite3.connect(pathDatabase)
+        action = connection.cursor()
+
+        # Update on startup
+        nextUpdate = updateManager.setUpdateInterval(updateManager.getGlobalTime(), interval_s=10)
+        action.execute('UPDATE repo SET nextcheck = "{0}" WHERE addonID ="repository.xpress"'.format(nextUpdate))
+        connection.commit()
+        action.execute('SELECT nextcheck FROM repo WHERE addonID ="repository.xpress"')
+        updateStart = action.fetchone()
+
+        # Update after 8 hours default time
+        nextUpdate = updateManager.setUpdateInterval(updateManager.getGlobalTime())
+        action.execute('UPDATE repo SET nextcheck = "{0}" WHERE addonID ="repository.xpress"'.format(nextUpdate))
+        connection.commit()
+        action.execute('SELECT nextcheck FROM repo WHERE addonID ="repository.xpress"')
+        updateHours = action.fetchone()
+
+        connection.close()
+
+    @staticmethod
+    def setUpdateInterval(timestamp, interval_h=8, interval_m=0, interval_s=0):
+        timestamp = ''.join(timestamp)
+        nextUpdate = datetime.datetime.strptime(timestamp, "%Y-%m-%d %H:%M:%S") + datetime.timedelta(hours=interval_h,
+                                                                                                     minutes=interval_m,
+                                                                                                     seconds=interval_s)
+        return str(nextUpdate)
+
+    @staticmethod
+    def getGlobalTime():
+        globalTime = datetime.datetime.now()
+        return str(globalTime.strftime("%Y-%m-%d %H:%M:%S"))
 
     """This function crawls through jsons to get the latest zip filename"""
 
@@ -93,10 +138,11 @@ class updateManager:
         if (addonFilenameZip is None):
             return None
         url = "https://github.com/{0}/{1}/blob/master/zips/{2}/{3}?raw=true".format(USER, REPOSITORY_NAME, ADDON_NAME,
-                                                                                addonFilenameZip)
+                                                                                    addonFilenameZip)
         return url
 
     """Checks if update is available"""
+
     @staticmethod
     def isUpdate():
         remoteVersion = updateManager.getRemoteVersion()
@@ -109,16 +155,21 @@ class updateManager:
             return False
 
     """Enable Addon"""
+
     @staticmethod
     def enableAddon():
-        xbmc.executeJSONRPC('{"jsonrpc":"2.0","method":"Addons.SetAddonEnabled","id":1,"params":{"addonid":"%s", "enabled":true}}' % ADDON_NAME)
+        xbmc.executeJSONRPC(
+            '{"jsonrpc":"2.0","method":"Addons.SetAddonEnabled","id":1,"params":{"addonid":"%s", "enabled":true}}' % ADDON_NAME)
 
     """Disable Addon"""
+
     @staticmethod
     def disableAddon():
-        xbmc.executeJSONRPC('{"jsonrpc":"2.0","method":"Addons.SetAddonEnabled","id":1,"params":{"addonid":"%s", "enabled":false}}' % ADDON_NAME)
+        xbmc.executeJSONRPC(
+            '{"jsonrpc":"2.0","method":"Addons.SetAddonEnabled","id":1,"params":{"addonid":"%s", "enabled":false}}' % ADDON_NAME)
 
     """Checks if addon.zip in packages exist"""
+
     @staticmethod
     def getCachedPackageName():
         arr = os.listdir(pathPackages)
@@ -128,14 +179,14 @@ class updateManager:
         return None
 
     """Remove cached addon.zip"""
+
     @staticmethod
     def clearCachedPackage():
         filename = updateManager.getCachedPackageName()
-        if (os.path.isfile(pathPackages+"\\{0}".format(filename))):
-            os.remove(pathPackages+"\\{0}".format(filename))
+        if (os.path.isfile(pathPackages + "\\{0}".format(filename))):
+            os.remove(pathPackages + "\\{0}".format(filename))
         else:
-            print("Error: %s file not found" % pathPackages+"\\{0}".format(filename))
-
+            print("Error: %s file not found" % pathPackages + "\\{0}".format(filename))
 
 # """Download latest version as .zip file"""
 # @staticmethod
