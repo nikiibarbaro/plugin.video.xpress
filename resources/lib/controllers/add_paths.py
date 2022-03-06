37e04c88627258dcfef03545226e80626f2d5904
nikiibarbaro <13036889+nikiibarbaro@users.noreply.github.com>
2022-03-06 16:25:07 +0100

    added global variables

--- resources/lib/controllers/add_paths.py
+++ resources/lib/controllers/add_paths.py
@@ -2,15 +2,26 @@ import xbmcaddon
 import xbmcvfs
 from sys import path
 from os.path import join
+import os
 
+global ADDON_NAME
 ADDON_NAME = "plugin.video.xpress"
 
+global addonPath
 addonPath = xbmcvfs.translatePath(xbmcaddon.Addon(ADDON_NAME).getAddonInfo('path'))
 
-class addPaths:
-    """Append paths"""
-    path.append(addonPath)
-    path.append(join(addonPath, "resources", "lib", "modules"))
-    path.append(join(addonPath, "resources", "lib", "models"))
-    path.append(join(addonPath, "resources", "lib", "sites"))
-    path.append(join(addonPath, "resources", "lib", "views"))
+global pathAddonDataAddon
+pathAddonDataAddon = xbmcvfs.translatePath(
+    join("special://home", "userdata", "addon_data", "{0}".format(ADDON_NAME)))
+
+os.makedirs(pathAddonDataAddon, exist_ok=True)
+global pathAddonSettings
+pathAddonSettings = xbmcvfs.translatePath(join(pathAddonDataAddon, "settings.json"))
+
+path.append(addonPath)
+path.append(join(addonPath, "art"))
+path.append(join(addonPath, "resources", "lib", "modules"))
+path.append(join(addonPath, "resources", "lib", "models"))
+path.append(join(addonPath, "resources", "lib", "sites"))
+path.append(join(addonPath, "resources", "lib", "views"))
+path.append(join(addonPath, "resources", "lib", "controllers"))
