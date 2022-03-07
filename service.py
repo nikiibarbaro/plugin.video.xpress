48fff13f6b02c5a6f272dfcd2cea499727300f66
nikiibarbaro <13036889+nikiibarbaro@users.noreply.github.com>
2022-03-07 21:15:34 +0100

    testing

--- service.py
+++ service.py
@@ -10,8 +10,9 @@ import os
 """Execute debugging"""
 # web_pdb.set_trace()  # NEED TO COMMENTED OUT BEFORE PUSHING TO GITHUB TO PREVENT UPDATER BREAKS
 
-#"""Required imports, appends controllers to sys.path in order that Kodi can import addon_add_paths.py"""
+"""Required imports, appends controllers to sys.path in order that Kodi can import addon_add_paths.py"""
 #from resources.lib.controllers import addon_add_paths
+#from resources.lib.controllers import addon_add_global_variables
 
 """Required imports"""
 from resources.lib.controllers.update_manager import updateManager
