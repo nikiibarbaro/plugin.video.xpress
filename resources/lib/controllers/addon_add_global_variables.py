ebbf17efeb61d2d842a5674d5dfb7f240c9e956a
nikiibarbaro <13036889+nikiibarbaro@users.noreply.github.com>
2022-03-07 20:57:47 +0100

    fixed small bug

--- resources/lib/controllers/addon_add_global_variables.py
+++ resources/lib/controllers/addon_add_global_variables.py
@@ -3,11 +3,6 @@ MAKE SURE TO IMPORT THIS FILE FIRST IN THE ROOT FILE TO ENSURE PROPER USE!
 File to set global variables and path appends to ensure the addon knows all the paths being used in classes
 """
 import os
-from resources.lib.controllers.logger import Logger
-
-
-"""Create logger"""
-logger = Logger(os.path.basename(__file__))
 
 global ADDON_ID
 ADDON_ID = "plugin.video.xpress"
@@ -20,5 +15,11 @@ REPOSITORY_NAME = "repository.xpress"
 
 global ADDON_PATH
 ADDON_PATH = "zips/plugin.video.xpress"
+
+from resources.lib.controllers.logger import Logger
+
+
+"""Create logger"""
+logger = Logger(os.path.basename(__file__))
 
 logger.debug("Global variables created")
