76c431c4b06b2f3100cd907aa55f98f46e13e6f0
nikiibarbaro <13036889+nikiibarbaro@users.noreply.github.com>
2022-03-09 15:22:00 +0100

    changed import paths

--- resources/lib/controllers/update_manager.py
+++ resources/lib/controllers/update_manager.py
@@ -7,12 +7,12 @@ import xml.etree.ElementTree as et
 
 # import web_pdb;#NEED TO COMMENTED OUT BEFORE PUSHING TO GITHUB TO PREVENT UPDATER BREAKS
 # web_pdb.set_trace()#NEED TO COMMENTED OUT BEFORE PUSHING TO GITHUB TO PREVENT UPDATER BREAKS
-from resources.lib.controllers.addon_add_global_variables import ADDON_ID, USER, REPOSITORY_NAME, ADDON_PATH
+from resources.lib.models.addon_add_global_variables import ADDON_ID, USER, REPOSITORY_NAME, ADDON_PATH
 
 """Add filepath from imports to addon"""
 from resources.lib.modules.packaging import version
-from resources.lib.controllers.settings import Settings
-from resources.lib.controllers.logger import Logger
+from resources.lib.models.settings import Settings
+from resources.lib.models.logger import Logger
 
 """Create logger"""
 logger = Logger(os.path.basename(__file__))
@@ -26,13 +26,13 @@ class updateManager:
         """Gets version from local addon"""
         try:
             addonVersionLocal = xbmcaddon.Addon(ADDON_ID).getAddonInfo("version")
-            logger.debug("Local version: " + addonVersionLocal)
+            logger.setDebugLog("Local version: " + addonVersionLocal)
         except RuntimeError:
-            logger.debug("Local version couldn't be fetched")
+            logger.setDebugLog("Local version couldn't be fetched")
             xbmc.sleep(4000)
             addonVersionLocal = xbmcaddon.Addon(ADDON_ID).getAddonInfo("version")
         if (addonVersionLocal is None):
-            logger.debug("Local version is None")
+            logger.setDebugLog("Local version is None")
             return None
         return addonVersionLocal
 
@@ -42,21 +42,21 @@ class updateManager:
         url = "https://raw.githubusercontent.com/{0}/{1}/master/zips/addons.xml".format(USER, REPOSITORY_NAME)
         response = requests.get(url)
         if (response.status_code != 200):
-            logger.debug("Couldn't get response")
+            logger.setDebugLog("Couldn't get response")
             return None
         tree = et.fromstring(response.content)
         for element in tree:
             if (element.attrib.get("id") == ADDON_ID):
                 remoteVersion = element.attrib.get("version")
-                logger.debug("Remote version: " + remoteVersion)
+                logger.setDebugLog("Remote version: " + remoteVersion)
                 return remoteVersion
-        logger.debug("Couldn't fetch remote version")
+        logger.setDebugLog("Couldn't fetch remote version")
         return None
 
     @staticmethod
     def forceRepoUpdate():
         """forces Kodi to check for new updates in the Repo and installing it"""
-        logger.debug("Force update")
+        logger.setDebugLog("Force update")
         xbmc.executebuiltin("UpdateAddonRepos")
 
     @staticmethod
@@ -67,37 +67,37 @@ class updateManager:
         if (remoteVersion is None or localVersion is None):
             return None
         if (version.parse(remoteVersion) > version.parse(localVersion)):
-            logger.debug("New update is available")
-            logger.debug("Old version: " + localVersion)
-            logger.debug("New version: " + remoteVersion)
+            logger.setDebugLog("New update is available")
+            logger.setDebugLog("Old version: " + localVersion)
+            logger.setDebugLog("New version: " + remoteVersion)
             return True
         else:
-            logger.debug("Addon is up to date")
+            logger.setDebugLog("Addon is up to date")
             return False
 
     @staticmethod
     def getIsUpdated():
         """Gets state whether on last run an update was applied to addon"""
-        logger.debug("Gets getIsUpdated state")
+        logger.setDebugLog("Gets getIsUpdated state")
         return Settings.getIsUpdated()
 
     @staticmethod
     def setIsUpdated(state):
         """Sets state of json 'isUpdated' to True/False"""
-        logger.debug("Sets setIsUpdated state: " + state)
+        logger.setDebugLog("Sets setIsUpdated state: " + state)
         Settings.setIsUpdated(state)
 
     @staticmethod
     def enableAddon():
         """Enables addon"""
-        logger.debug("Addon enabled")
+        logger.setDebugLog("Addon enabled")
         xbmc.executeJSONRPC(
             '{"jsonrpc":"2.0","method":"Addons.SetAddonEnabled","id":1,"params":{"addonid":"%s", "enabled":true}}' % ADDON_ID)
 
     @staticmethod
     def disableAddon():
         """Disables addon"""
-        logger.debug("Addon disabled")
+        logger.setDebugLog("Addon disabled")
         xbmc.executeJSONRPC(
             '{"jsonrpc":"2.0","method":"Addons.SetAddonEnabled","id":1,"params":{"addonid":"%s", "enabled":false}}' % ADDON_ID)
 
